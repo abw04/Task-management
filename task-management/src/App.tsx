6ad023e149bee0f213ddc7646d99b8e81ffb8c20
unknown <ardanuw@gmail.com>
2026-06-01 16:52:34 +0700

    display task card and check

--- task-management/src/App.tsx
+++ task-management/src/App.tsx
@@ -1,5 +1,13 @@
+import { TaskCard } from "./modules/components/task-card";
+import { TaskData } from "./modules/task/task.data";
+
 export default function App() {
   return (
-  <h1>Task Management</h1>
+    <div>
+      <h1>Task Management</h1>
+      {TaskData.map((task) =>{
+        return <TaskCard key = {task.id} task ={task} />
+      })}
+    </div>
   );
 }
--- task-management/src/modules/components/task-card.tsx
+++ task-management/src/modules/components/task-card.tsx
@@ -5,8 +5,11 @@ export function TaskCard({task}:{task:Task}) {
     return(
         <div>
             <h2>
-                <TaskCheck task = {task} /> {task.title}
+                <TaskCheck task = {task} /> {task.title} 
             </h2>
+            <h3>
+                {task.description}
+            </h3>
         </div>
     )
 }
