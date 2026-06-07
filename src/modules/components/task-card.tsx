0b12f1c5339f0e95ef096f53b48f20bd9a803f79
unknown <ardanuw@gmail.com>
2026-06-07 09:10:00 +0700

    apply prettier to improve readability

--- src/modules/components/task-card.tsx
+++ src/modules/components/task-card.tsx
@@ -1,16 +1,13 @@
 import type { Task } from "../task/task.type";
 import { TaskCheck } from "./task-check";
 
-export function TaskCard({task}:{task:Task}) {
-    return(
-        
-        <div className ="border bg-orange-200 p-2 m-2">
-            <h2 className="text-xl">
-                <TaskCheck task = {task} /> {task.title} 
-            </h2>
-            <h3 className="text-sm">
-                {task.description}
-            </h3>
-        </div>
-    )
+export function TaskCard({ task }: { task: Task }) {
+  return (
+    <div className="border bg-orange-200 p-2 m-2">
+      <h2 className="text-xl">
+        <TaskCheck task={task} /> {task.title}
+      </h2>
+      <h3 className="text-sm">{task.description}</h3>
+    </div>
+  );
 }
--- src/modules/task/task.data.tsx
+++ src/modules/task/task.data.tsx
@@ -1,22 +1,22 @@
 import type { Task } from "./task.type";
 
 export const TaskData: Task[] = [
-    {
-        id: 1,
-        title: "Exercise",
-        description: "jogging",
-        isCompleted: false
-    },
-    {
-        id: 2,
-        title: "Working",
-        description: "office time",
-        isCompleted: true
-    },
-    {
-        id: 1,
-        title: "Upskilling",
-        description: "Bearmentor tasks",
-        isCompleted: true
-    },
-]
+  {
+    id: 1,
+    title: "Exercise",
+    description: "jogging",
+    isCompleted: false,
+  },
+  {
+    id: 2,
+    title: "Working",
+    description: "office time",
+    isCompleted: true,
+  },
+  {
+    id: 1,
+    title: "Upskilling",
+    description: "Bearmentor tasks",
+    isCompleted: true,
+  },
+];
