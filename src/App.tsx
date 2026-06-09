5da2d51b022bcc83466fc77f32767d902940f941
unknown <ardanuw@gmail.com>
2026-06-09 17:24:35 +0700

    improve coloring

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import { tasksData } from "./modules/task/task.data";
 
 export default function App() {
   return (
-    <div className="max-w-xl mx-auto bg-orange-300 min-h-screen">
-      <h1 className="text-3xl mx-2 pt-4 text-center font-bold font-heading">
+    <div className="max-w-xl mx-auto bg-[oklch(0.45_0.085_224.283)] min-h-screen">
+      <h1 className="text-3xl mx-2 pt-4 text-center font-bold font-heading text-[oklch(0.984_0.019_200.873)]">
         Task Management
       </h1>
       <div>
